9e519cbccc95cdf380b7153f670789e370c9a219
alisondiegodev <alisondiego@live.com>
2023-02-09 20:45:26 -0300

    comit

--- add_compra.php
+++ add_compra.php
@@ -1,3 +1,46 @@
 <?php
+session_start();
 include_once 'config.php';
-dsdas;
+
+
+if (!empty($_GET['id'])) {
+
+    $id = $_GET['id'];
+
+    $query = "SELECT * FROM produtos WHERE id_produto = $id";
+
+    $result = $con->query($query);
+
+    if ($result->num_rows > 0) {
+        $produto_info = mysqli_fetch_assoc($result);
+
+        $nome_produto = $produto_info['nome_produto'];
+        $valor = $produto_info['valor'];
+    }
+}
+
+
+if (!empty($_SESSION['email'])) {
+
+    $user_email = $_SESSION['email'];
+
+    $sql = "SELECT * FROM usuarios WHERE email ='$user_email'";
+
+    $sql_result = $con->query($sql);
+
+    if ($sql_result->num_rows > 0) {
+        $user = mysqli_fetch_assoc($sql_result);
+
+        $usuario_id = $user['id'];
+        $usuario_nome = $user['nome'];
+    }
+}
+
+
+if (!empty($_SESSION['email'] && !empty($_GET['id']))) {
+
+    $sqlCompra = "INSERT INTO compras (item_pedido,valor_pedido,id_usuario,nome_usuario) VALUES ('$nome_produto', '$valor', '$usuario_id', '$usuario_nome')";
+    $compraResult = $con->query($sqlCompra);
+
+    header('Location: usuario.php');
+}
--- usuario.php
+++ usuario.php
@@ -18,6 +18,7 @@ $result = $con->query($sql);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sistema</title>
   <link rel="stylesheet" href="css/styleRead.css">
+  <link rel="stylesheet" href="global.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   <style>
 
@@ -25,35 +26,35 @@ $result = $con->query($sql);
 </head>
 
 <body>
-  <div class="flex-rule">
-    <main>
-      <a class="logout" href="logout.php"> Logout</a>
-      <h1> BEM VINDO
-      </h1>
-      <p>Bem vindo Alien!</p>
+  <div class="container">
+    <div class="flex-rule">
+      <main>
+        <a class="logout" href="logout.php"> Logout</a>
+
+        <p>Bem vindo Alien!</p>
 
 
-      <table class="table caption-top" id=table>
-        <caption>FAZER NOVA COMPRA</caption>
-        <thead>
-          <tr>
-            <th scope="col"></th>
-            <th scope="col">Item</th>
-            <th scope="col">Preço</th>
-            <th scope="col">Categoria</th>
-            <th scope="col">Comprar</th>
+        <table class="table caption-top" id=table>
+          <caption>FAZER NOVA COMPRA</caption>
+          <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col">Item</th>
+              <th scope="col">Preço</th>
+              <th scope="col">Categoria</th>
+              <th scope="col">Comprar</th>
 
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          while ($produtos_lista = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>" . $produtos_lista['id_produto'] . "</td>";
-            echo "<td>" . $produtos_lista['nome_produto'] . "</td>";
-            echo "<td>" . $produtos_lista['valor'] . "</td>";
-            echo "<td>" . $produtos_lista['id_categoria'] . "</td>";
-            echo "<td>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            while ($produtos_lista = mysqli_fetch_assoc($result)) {
+              echo "<tr>";
+              echo "<td>" . $produtos_lista['id_produto'] . "</td>";
+              echo "<td>" . $produtos_lista['nome_produto'] . "</td>";
+              echo "<td>" . $produtos_lista['valor'] . "</td>";
+              echo "<td>" . $produtos_lista['id_categoria'] . "</td>";
+              echo "<td>
                     <a class= 'btn btn-primary' href='./add_compra.php?id=$produtos_lista[id_produto]'>
                     <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-cart-check' viewBox='0 0 16 16'>
   <path d='M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z'/>
@@ -62,21 +63,18 @@ $result = $con->query($sql);
                   </a>
                   
                   </td>";
-            echo "</tr>";
-          }
+              echo "</tr>";
+            }
 
 
-          ?>
-        </tbody>
-      </table>
+            ?>
+          </tbody>
+        </table>
 
 
-    </main>
-    <section class="images">
+      </main>
 
-
-
-    </section>
+    </div>
   </div>
 
 </body>
