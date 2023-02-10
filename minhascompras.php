052de5aebe2f51333e6ab106b7ec9fa56791a73c
alisondiegodev <alisondiego@live.com>
2023-02-09 23:01:43 -0300

    buy

---
+++ minhascompras.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+require_once 'config.php';
+if (!isset($_SESSION['email']) || !isset($_SESSION['senha'])) {
+
+    header('Location: login.php');
+}
+
+if (!empty($_SESSION['email'])) {
+
+    $user_email = $_SESSION['email'];
+
+    $sql = "SELECT * FROM usuarios WHERE email ='$user_email'";
+
+    $sql_result = $con->query($sql);
+
+    if ($sql_result->num_rows > 0) {
+        $user = mysqli_fetch_assoc($sql_result);
+
+        $usuario_id = $user['id'];
+        $usuario_nome = $user['nome'];
+    }
+}
+
+if (isset($usuario_id)) {
+    $sql_select = "SELECT * FROM compras WHERE id_usuario = '$usuario_id'";
+    $result = $con->query($sql_select);
+}
+
+?>
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema</title>
+    <link rel="stylesheet" href="css/styleRead.css">
+    <link rel="stylesheet" href="global.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="flex-rule">
+            <main>
+                <div class="header">
+                    <p>Bem Vindo <?= $usuario_nome ?>!</p>
+                    <div>
+                        <a class="botao" href="./usuario.php">Inicio</a>
+                        <a class="botao" href="logout.php"> Logout</a>
+
+                    </div>
+                </div>
+
+                <table class="table caption-top" id=table>
+                    <caption>FAZER NOVA COMPRA</caption>
+                    <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Item</th>
+                            <th scope="col">Preço</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        while ($compras = mysqli_fetch_assoc($result)) {
+                            echo "<tr>";
+                            echo "<td style='display:hidden;'>" . $compras['id_pedido'] . "</td>";
+                            echo "<td>" . $compras['item_pedido'] . "</td>";
+                            echo "<td>" . $compras['valor_pedido'] . "</td>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+
+
+            </main>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
